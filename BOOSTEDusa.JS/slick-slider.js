2d9983498a6139aa23c6b40d23a957336a440f99
King829282 <106445844+King829282@users.noreply.github.com>
2022-07-25 20:16:03 +0600

    Add files via upload

---
+++ BOOSTEDusa.JS/slick-slider.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    $('.accessories-list').slick({
+        arrows: true,
+        adaptiveHeight: true,
+        slidesToShow: 6,
+        slidesToScroll: 2,
+        speed: 800,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        pauseOnFocus: false,
+        pauseOnHover: false,
+        swipe: true,
+        touchThreshold: 15,
+        touchMove: true,
+        waitForAnimate: false,
+
+    });
+})
